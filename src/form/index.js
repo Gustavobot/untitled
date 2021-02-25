5d5d284a43f78b119c9e5e1a60ff715276e8cb59
Gustavo <4500114059@estudante.sed.sc.gov.br>
2021-02-25 16:04:46 -0300

    ajuste layout

--- src/form/index.js
+++ src/form/index.js
@@ -1,5 +1,5 @@
 import {useState} from "react";
-import {Button, Grid, TextField} from "@material-ui/core";
+import {Button, Grid, Paper, TextField} from "@material-ui/core";
 import {useForm,Controller} from "react-hook-form";
 import InputMask from 'react-input-mask';
 
@@ -21,68 +21,71 @@ const FormComponent = () => {
     }
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-        <Grid container spacing={2}>
+        <Paper>
+            <form onSubmit={handleSubmit(onSubmit)}>
+            <Grid container spacing={2}>
+                <Grid item>
+                    <TextField
+                        error={!!errors['cep']}
+                        helperText={errors['cep']?.message}
+                        inputRef={register({ required: { value: true, message: "campo obrigatorio" }, maxLength: 8})}
+                        label="Cep"
+                        name={'cep'}
+                        onChange={(e) => {
+
+                            if (e.target.value.length==8){
+                            PesquisarCep(e.target.value)
+                        }
+                    }}/>
+                </Grid>
             <Grid item>
                 <TextField
-                    error={!!errors['cep']}
-                    helperText={errors['cep']?.message}
-                    inputRef={register({ required: { value: true, message: "campo obrigatorio" }, maxLength: 8})}
-                    label="Cep"
-                    name={'cep'}
-                    onChange={(e) => {
-
-                        if (e.target.value.length==8){
-                        PesquisarCep(e.target.value)
-                    }
-                }}/>
-            </Grid>
-        <Grid item>
-            <TextField
-                name={'fone'}
-                label="Telefone"
-                inputRef={register}
-            />
-        </Grid>
-            <Grid item>
-            <TextField
-                name={'localidade'}
-                label="Cidade"
-                disabled
-                // value={state.data?.localidade}
-                inputRef={register}
-            />
-        </Grid>
-        {/*<Grid item>*/}
-        {/*    <TextField*/}
-        {/*        name={'cpf'}*/}
-        {/*        label="CPF"*/}
-        {/*        inputRef={register}*/}
-        {/*    />*/}
-        {/*</Grid>*/}
-        <Grid item>
-            <Button color="secondary" variant={'contained'} type={'submit'}>Enviar</Button>
-        </Grid>
-        </Grid>
-            <Grid>
-                <Controller
-                    as={
-                        // <CustomInputMask field={field} errors={errors} />
-                        <InputMask mask={'999.999.999-99'} >
-                            {() => (
-                                <TextField
-                                    error={!!errors['cpf']}
-                                    name={'cpf'}
-                                    label={'CPF'}
-                                    helperText={errors['cpf']?.message}
-                                />
-                            )}
-                        </InputMask>
-                    }
-                    control={control}
-                    name={'cpf'}
+                    name={'fone'}
+                    label="Telefone"
+                    inputRef={register}
                 />
             </Grid>
-        </form>)
+                <Grid item>
+                <TextField
+                    name={'localidade'}
+                    label="Cidade"
+                    disabled
+                    // value={state.data?.localidade}
+                    inputRef={register}
+                />
+            </Grid>
+            {/*<Grid item>*/}
+            {/*    <TextField*/}
+            {/*        name={'cpf'}*/}
+            {/*        label="CPF"*/}
+            {/*        inputRef={register}*/}
+            {/*    />*/}
+            {/*</Grid>*/}
+            </Grid>
+                <Grid>
+                    <Controller
+                        as={
+                            // <CustomInputMask field={field} errors={errors} />
+                            <InputMask mask={'999.999.999-99'} >
+                                {() => (
+                                    <TextField
+                                        error={!!errors['cpf']}
+                                        name={'cpf'}
+                                        label={'CPF'}
+                                        helperText={errors['cpf']?.message}
+                                    />
+                                )}
+                            </InputMask>
+                        }
+                        control={control}
+                        name={'cpf'}
+                    />
+                </Grid>
+                <Grid item>
+                    <Button color="secondary" variant={'contained'} type={'submit'}>Enviar</Button>
+                </Grid>
+            </form>
+        </Paper>
+    )
 }
 export default FormComponent
--- src/home/index.js
+++ src/home/index.js
@@ -1,4 +1,4 @@
-import {Avatar, Box, Grid, ListItem, ListItemText, makeStyles} from "@material-ui/core";
+import {Avatar, Box, Grid, ListItem, ListItemText, makeStyles, Paper} from "@material-ui/core";
 import foto from './foto.jpg';
 import List from "@material-ui/core/List";
 
@@ -9,20 +9,22 @@ const HomeComponent = () => {
     const Cursos = ['HTML5', 'CSS3', 'JS','REACT']
 
     return(
-        <Grid container justify={'center'} direction={'column'} alignItems={'center'}>
-            <Avatar className={classes.large} src={foto}/>
-            <h1>Gustavo Borges</h1>
-            <Box>
-               <p>Saudações</p>
-                <p>Sou aluno do ensino médio e tenho estudado tecnologia no ultimo ano, neste tempo desenvolvi algumas atividades para colocar em pratica o conteudo que aprendi.</p>
-                <p>Dentre os cursos que fiz estão:</p>
-                <List component="nav" aria-label="secondary mailbox folders">
-                    {Cursos.map(item => (<ListItem button>
-                        <ListItemText primary={item} />
-                    </ListItem>))}
-                </List>
-            </Box>
-        </Grid>
+            <Grid container justify={'center'} direction={'column'} alignItems={'center'}>
+                <Avatar className={classes.large} src={foto}/>
+                <h1>Gustavo Borges</h1>
+                <Paper elevation={2}>
+                    <Box p={3}>
+                       <p>Saudações</p>
+                        <p>Sou aluno do ensino médio e tenho estudado tecnologia no ultimo ano, neste tempo desenvolvi algumas atividades para colocar em pratica o conteudo que aprendi.</p>
+                        <p>Dentre os cursos que fiz estão:</p>
+                        <List component="nav" aria-label="secondary mailbox folders">
+                            {Cursos.map(item => (<ListItem button>
+                                <ListItemText primary={item} />
+                            </ListItem>))}
+                        </List>
+                    </Box>
+                </Paper>
+            </Grid>
     )
 }
 
